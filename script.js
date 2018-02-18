4c309f5f879e5016dc40bd1eb947aa11b58ead07
Vinglish <khan.ildar94@gmail.com>
2018-02-19 00:06:00 +0200

    Coffee

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-var money = Number(prompt("Enter the money. from UAH 5 to UAH 20"));
-var drink =prompt("Choose a drink: if you want Espresso(UAH 5); Americano(UAH 10); Late(UAH 15)");
+var money = Number(prompt("Enter the money. from 5 UAH to 20 UAH"));
+var drink =prompt("Choose a drink: if you want Espresso(5 UAH); Americano(10 UAH); Late(15 UAH)");
 if(drink=="Espresso"){
 	if(money < 5){
-	var sum = Number(prompt("not enought money"));
+	var sum = Number(prompt("not enought money. Cost of espresso 5 UAH. Add" + (5-money) + "UAH"));
 	money += sum;}
 if(money>=5){
 	alert("Your choose Espresso. Your deliver:" + (money-5));
 }}
 else if(drink=="Americano"){
 	if(money < 10){
-	var sum = Number(prompt("not enought money"));
+	var sum = Number(prompt("not enought money. Cost of americano 10 UAH. Add" + (10-money) + "UAH"));
 	money += sum;}
 if(money>=10){
 	alert("Your choose Americano. Your deliver:" + (money-10));
 }}
 else if(drink=="Late"){
 	if(money < 15){
-	var sum = Number(prompt("not enought money"));
+	var sum = Number(prompt("not enought money. Cost of late 15 UAH. Add" + (15-money) + "UAH"));
 	money += sum;}
 if(money>=15){
 	alert("Your choose Late. Your deliver:" + (money-15));
